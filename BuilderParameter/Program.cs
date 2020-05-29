1cea891e5b8506b9ffb75be2f255a3b4c2b19ffd
D3ad5h0t <alexgl007@mail.ru>
2020-05-29 23:31:08 +1000

    builder parameter

---
+++ BuilderParameter/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BuilderParameter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var emailService = new EmailService();
+            emailService.SendEmail(builder => 
+                builder.From("abc@xyz.com")
+                    .To("qwe@rty.com")
+                    .Subject("Test")
+                    .Body("Hello World"));
+        }
+    }
+}
